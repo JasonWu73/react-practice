07aee4480af8b9251577eca11d3a3564ff5a0396
吴仙杰 <wuxianjiezh@gmail.com>
2024-01-21 13:16:13 +0800

    add post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { Post } from '@/posts/Post'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Post/>
   )
 }
---
+++ src/posts/Post.tsx
@@ -0,0 +1,8 @@
+export function Post() {
+  return (
+    <div>
+      <p>Jason Wu</p>
+      <p>React.js is awesome!</p>
+    </div>
+  )
+}
