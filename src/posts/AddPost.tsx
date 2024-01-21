6e34986139c45595de1c93abe29453b0d89626aa
吴仙杰 <wuxianjiezh@gmail.com>
2024-01-21 20:12:29 +0800

    lifting state

--- src/posts/AddPost.tsx
+++ src/posts/AddPost.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
+type AddPostProps = {
+  onChangeText: (text: string) => void
+  onChangeAuthor: (author: string) => void
+}
 
-export function AddPost() {
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-  }
-
+export function AddPost({ onChangeText, onChangeAuthor }: AddPostProps) {
   return (
     <form
-      onSubmit={handleSubmit}
       className="mx-auto mt-8 p-4 space-y-4 max-w-md border border-amber-500 rounded shadow shadow-amber-500"
     >
       <div className="flex flex-col gap-2">
         <label htmlFor="text" className="w-24 font-semibold">Text</label>
         <textarea
+          onChange={e => onChangeText(e.target.value)}
           id="text"
           rows={5}
           className="flex-grow px-2 py-1 bg-slate-600 rounded focus:outline-none focus:ring-1 focus:ring-amber-500"
@@ -23,6 +21,7 @@ export function AddPost() {
       <div className="flex flex-col gap-2">
         <label htmlFor="author" className="w-24 font-semibold">Your name</label>
         <input
+          onChange={e => onChangeAuthor(e.target.value)}
           type="text"
           id="author"
           className="flex-grow px-2 py-1 bg-slate-600 rounded focus:outline-none focus:ring-1 focus:ring-amber-500"
--- src/posts/PostList.tsx
+++ src/posts/PostList.tsx
@@ -1,13 +1,26 @@
+import React from 'react'
+
 import { Post } from '@/posts/Post'
 import { AddPost } from '@/posts/AddPost'
 
 export function PostList() {
+  const [text, setText] = React.useState('')
+  const [author, setAuthor] = React.useState('')
+
+  function handleChangeText(text: string) {
+    setText(text)
+  }
+
+  function handleChangeAuthor(author: string) {
+    setAuthor(author)
+  }
+
   return (
     <>
-      <AddPost/>
+      <AddPost onChangeText={handleChangeText} onChangeAuthor={handleChangeAuthor}/>
 
       <ul className="flex justify-center gap-4 flex-wrap mt-8">
-        <Post author="Jason" text="React.js is awesome"/>
+        <Post author={author} text={text}/>
         <Post author="Bruce" text="Check out the full course"/>
       </ul>
     </>
