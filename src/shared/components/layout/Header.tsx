ab2f410f0365c48d997301bec7f9717eac71b6c6
吴仙杰 <wuxianjiezh@gmail.com>
2024-01-22 21:34:57 +0800

    使用 fontawesome icon

--- src/shared/components/layout/Header.tsx
+++ src/shared/components/layout/Header.tsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment } from '@fortawesome/free-regular-svg-icons'
+import { faCommentMedical } from '@fortawesome/free-solid-svg-icons'
+
 type HeaderProps = {
   onAddPost: () => void
 }
@@ -5,13 +9,17 @@ type HeaderProps = {
 export function Header({ onAddPost }: HeaderProps) {
   return (
     <header className="flex justify-between mx-auto mt-8 w-10/12 border-b-2 border-amber-500">
-      <h1 className="pb-4 text-2xl font-bold">React Poster</h1>
+      <h1 className="pb-4 text-2xl font-bold">
+        <FontAwesomeIcon icon={faComment} className="mr-2"/>
+        <span>React Poster</span>
+      </h1>
       <div>
         <button
           onClick={onAddPost}
           className="px-2 py-1 border border-amber-500 rounded bg-slate-600 font-bold hover:bg-slate-700 focus:outline-none focus:ring-1 focus:ring-amber-400"
-        >Add
-          Post
+        >
+          <FontAwesomeIcon icon={faCommentMedical} className="mr-2"/>
+          <span>Add Post</span>
         </button>
       </div>
     </header>
