abace1f03aba3e9e5fad2683ed2c9ac4235186c1
吴仙杰 <wuxianjiezh@gmail.com>
2024-01-25 21:16:29 +0800

    15:04

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { RouterProvider } from 'react-router-dom'
-
-import { routes } from '@/routes'
-
 export default function App() {
-
   return (
-    <RouterProvider router={routes}/>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
